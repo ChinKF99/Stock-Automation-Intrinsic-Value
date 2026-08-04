6ca0f3647c7363f1e8d312a25b0aa14de451b325
ChinKF99 <136291127+ChinKF99@users.noreply.github.com>
2026-08-04 08:32:08 +0800

    Categorize the script into general, bronze and silver
    -Carry out categorization for ease of reference in the future.
    -To rename certain scripts for better naming reference such as (bulk_insert_dataframe > append_dataframe)

--- utils/sql_utils.py
+++ utils/sql_utils.py
@@ -13,8 +13,9 @@ import sys
 from sqlalchemy import text
 from sqlalchemy.engine import Engine
 import pandas as pd
-from config.logging_config import setup_logger
 
+
+from config.logging_config import setup_logger
 # 1. Get the base name (e.g., "script.py")
 raw_name = os.path.basename(sys.argv[0])
 
@@ -23,6 +24,37 @@ log_name = os.path.splitext(raw_name)[0]
 
 # 3. Initialize the logger
 logger = setup_logger(log_name)
+
+# ==========================================================
+# SQL Schema & Tables
+# ==========================================================
+
+# Schemas
+BRONZE_SCHEMA = "bronze"
+SILVER_SCHEMA = "silver"
+
+# Bronze Tables
+BRONZE_02_TABLE = "sp500_tickers"
+BRONZE_02_SCHEMA_TABLE = f"{BRONZE_SCHEMA}.{BRONZE_02_TABLE}"
+BRONZE_04_TABLE = "company_profile"
+BRONZE_04_SCHEMA_TABLE = f"{BRONZE_SCHEMA}.{BRONZE_04_TABLE}"
+BRONZE_06_TABLE = 'income_statement'
+BRONZE_06_SCHEMA_TABLE = f"{BRONZE_SCHEMA}.{BRONZE_06_TABLE}"
+BRONZE_08_TABLE = 'balance_sheet'
+BRONZE_08_SCHEMA_TABLE = f"{BRONZE_SCHEMA}.{BRONZE_08_TABLE}"
+BRONZE_10_TABLE = 'cash_flow'
+BRONZE_10_SCHEMA_TABLE = f"{BRONZE_SCHEMA}.{BRONZE_10_TABLE}"
+BRONZE_12_TABLE = 'ratios_ttm'
+BRONZE_12_SCHEMA_TABLE = f"{BRONZE_SCHEMA}.{BRONZE_12_TABLE}"
+
+# Silver Tables
+SILVER_01_TABLE = 'company_financials'
+SILVER_01_SCHEMA_TABLE = f"{SILVER_SCHEMA}.{SILVER_01_TABLE}"
+
+
+# *****************************************************************************
+# UNIVERSAL SQL SCRIPTS
+# *****************************************************************************
 
 # ==========================================================
 # Connection Information
@@ -48,34 +80,6 @@ def print_connection_info(engine: Engine) -> None:
     logger.info(f"Server   : {row.server_name}")
     logger.info(f"Database : {row.database_name}")
     logger.info(f"Login    : {row.login_name}")
-
-# ==========================================================
-# SQL Scheme & Tables
-# ==========================================================
-
-#Bronze table for bronze.sp500_tickers
-
-# Schemas
-BRONZE_SCHEMA = "bronze"
-SILVER_SCHEMA = "silver"
-
-# Bronze Tables
-BRONZE_02_TABLE = "sp500_tickers"
-BRONZE_02_SCHEMA_TABLE = f"{BRONZE_SCHEMA}.{BRONZE_02_TABLE}"
-BRONZE_04_TABLE = "company_profile"
-BRONZE_04_SCHEMA_TABLE = f"{BRONZE_SCHEMA}.{BRONZE_04_TABLE}"
-BRONZE_06_TABLE = 'income_statement'
-BRONZE_06_SCHEMA_TABLE = f"{BRONZE_SCHEMA}.{BRONZE_06_TABLE}"
-BRONZE_08_TABLE = 'balance_sheet'
-BRONZE_08_SCHEMA_TABLE = f"{BRONZE_SCHEMA}.{BRONZE_08_TABLE}"
-BRONZE_10_TABLE = 'cash_flow'
-BRONZE_10_SCHEMA_TABLE = f"{BRONZE_SCHEMA}.{BRONZE_10_TABLE}"
-BRONZE_12_TABLE = 'ratios_ttm'
-BRONZE_12_SCHEMA_TABLE = f"{BRONZE_SCHEMA}.{BRONZE_12_TABLE}"
-
-# Silver Tables
-SILVER_01_TABLE = 'company_financials'
-SILVER_01_SCHEMA_TABLE = f"{SILVER_SCHEMA}.{SILVER_01_TABLE}"
 
 # ==========================================================
 # Ensure Schema
@@ -115,72 +119,6 @@ def ensure_schema(
 
     logger.info("Verified Schema %s", schema)
 
-# ==============================================================================
-# Create Table from scratch with known schemas (For Bronze Layer only)
-# ==============================================================================
-
-def ensure_table(engine, schema, table, create_sql):
-    """    
-
-    engine : SQLAlchemy Engine
-
-    schema : str
-        Schema name.
-        Example:
-            bronze
-
-    table : str
-    Table name only.
-    Example:
-        sp500_tickers
-
-    create_sql : str
-    CREATE TABLE statement only.
-
-    Example:
-
-    CREATE TABLE bronze.sp500_tickers
-    (
-        ...
-    );"""
-
-    sql = f"""
-        IF OBJECT_ID('{schema}.{table}','U') IS NULL
-        BEGIN
-            {create_sql}
-        END;
-        """
-    
-    with engine.begin() as conn:
-        conn.execute(text(sql))
-
-    logger.info("Verified Schema Table %s.%s", schema, table)
-
-# ==========================================================
-# Check if the table exists (For Silver & Gold Layer)
-# ==========================================================
-
-def table_exists(engine, schema, table):
-
-    sql = text("""
-        SELECT 1
-        FROM INFORMATION_SCHEMA.TABLES
-        WHERE TABLE_SCHEMA = :schema
-          AND TABLE_NAME = :table
-    """)
-
-    with engine.begin() as conn:
-
-        result = conn.execute(
-            sql,
-            {
-                "schema": schema,
-                "table": table
-            }
-        )
-
-        return result.first() is not None
-    
 # ==========================================================
 # Get Row Count
 # ==========================================================
@@ -195,7 +133,6 @@ def get_row_count(engine, schema: str, table_name: str) -> int:
         count = conn.execute(sql).scalar()
     
     logger.info(f"Total {count} rows inserted")
-
 
 # ==========================================================
 # Truncate Table
@@ -234,7 +171,37 @@ def get_sp500_tickers(scheme_table, engine):
     return df["ticker"].tolist()
 
 # ==========================================================
-# Bulk Insert DataFrame
+# Load SQL Table
+# ==========================================================
+
+def load_table(schema_table, engine):
+    query = f"""
+    SELECT *
+    FROM {schema_table}
+    """
+    return pd.read_sql(query, engine)
+
+# ==========================================================
+# Select only the required columns from a DataFrame.
+# ==========================================================
+
+def select_columns(df, columns, table_name):
+    """
+    Raises:
+        KeyError: If one or more requested columns do not exist.
+    """
+
+    missing_columns = set(columns) - set(df.columns)
+
+    if missing_columns:
+        raise KeyError(
+            f"{table_name} is missing columns: {sorted(missing_columns)}"
+        )
+
+    return df[columns].copy()
+
+# ==========================================================
+# Append DataFrame into existing created SQL Table
 # ==========================================================
 
 def bulk_insert_dataframe(
@@ -264,8 +231,82 @@ def bulk_insert_dataframe(
     logger.info("Insert completed.")
 
 
+# *****************************************************************************
+# BRONZE LAYER SQL SCRIPTS
+# *****************************************************************************
+
 # ==========================================================
-# Test
+# Create Table from scratch with known Schemas & Columns
+# ==========================================================
+
+def ensure_table(engine, schema, table, create_sql):
+    """    
+
+    engine : SQLAlchemy Engine
+
+    schema : str
+        Schema name.
+        Example:
+            bronze
+
+    table : str
+    Table name only.
+    Example:
+        sp500_tickers
+
+    create_sql : str
+    CREATE TABLE statement only.
+
+    Example:
+
+    CREATE TABLE bronze.sp500_tickers
+    (
+        ...
+    );"""
+
+    sql = f"""
+        IF OBJECT_ID('{schema}.{table}','U') IS NULL
+        BEGIN
+            {create_sql}
+        END;
+        """
+    
+    with engine.begin() as conn:
+        conn.execute(text(sql))
+
+    logger.info("Verified Schema Table %s.%s", schema, table)
+
+# *****************************************************************************
+# SILVER LAYER SQL SCRIPTS
+# *****************************************************************************
+
+# ==========================================================
+# Check if the table exists (For Silver & Gold Layer)
+# ==========================================================
+
+def table_exists(engine, schema, table):
+
+    sql = text("""
+        SELECT 1
+        FROM INFORMATION_SCHEMA.TABLES
+        WHERE TABLE_SCHEMA = :schema
+          AND TABLE_NAME = :table
+    """)
+
+    with engine.begin() as conn:
+
+        result = conn.execute(
+            sql,
+            {
+                "schema": schema,
+                "table": table
+            }
+        )
+
+        return result.first() is not None
+
+# ==========================================================
+# Append dataframe after checking if table existed
 # ==========================================================
 
 def load_dataframe_to_sql(
@@ -274,6 +315,10 @@ def load_dataframe_to_sql(
     schema,
     table
 ):
+    """
+    To check if table with data already existed
+    This is to avoid double entry of data.
+    """
     if table_exists(engine, schema, table):
         truncate_table(engine, schema, table)
         bulk_insert_dataframe(engine, dataframe, schema, table)
@@ -290,37 +335,7 @@ def load_dataframe_to_sql(
         )
 
 # ==========================================================
-# Load SQL Table
-# ==========================================================
-
-def load_table(schema_table, engine):
-    query = f"""
-    SELECT *
-    FROM {schema_table}
-    """
-    return pd.read_sql(query, engine)
-
-# ==========================================================
-# Select only the required Silver columns from a DataFrame.
-# ==========================================================
-
-def select_columns(df, columns, table_name):
-    """
-    Raises:
-        KeyError: If one or more requested columns do not exist.
-    """
-
-    missing_columns = set(columns) - set(df.columns)
-
-    if missing_columns:
-        raise KeyError(
-            f"{table_name} is missing columns: {sorted(missing_columns)}"
-        )
-
-    return df[columns].copy()
-
-# ==========================================================
-# Define Silver Schema
+# Define column needed for step 13_build_company_financials
 # ==========================================================
 
 PROFILE_COLUMNS = [
